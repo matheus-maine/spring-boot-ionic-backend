5ef028ee28bbc90ef5ef0ddd53107a9b8c5e07db
Matheus Maine <matheusengcomp@hotmail.com>
2020-07-07 15:36:12 -0300

    Estado e cidade

---
+++ src/main/java/com/matheusmaine/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.matheusmaine.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheusmaine.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
