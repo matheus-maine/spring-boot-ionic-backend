ced9c6b9c3dd7e31375c6061b642221b0291cf4a
Matheus Maine <matheusengcomp@hotmail.com>
2020-07-07 11:20:16 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/matheusmaine/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.matheusmaine.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheusmaine.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
