8192b0059d11c9e4265f30cac384d02645464538
Matheus Maine <matheusengcomp@hotmail.com>
2020-07-27 14:57:10 -0300

    Inserindo uma nova Categoria

--- src/main/java/com/matheusmaine/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/matheusmaine/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.matheusmaine.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.matheusmaine.cursomc.domain.Categoria;
 import com.matheusmaine.cursomc.services.CategoriaService;
@@ -23,6 +28,14 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
 		Categoria cat = categoriaService.buscarCategoria(id);
 		return ResponseEntity.ok().body(cat);
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = categoriaService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/matheusmaine/cursomc/services/CategoriaService.java
+++ src/main/java/com/matheusmaine/cursomc/services/CategoriaService.java
@@ -23,4 +23,8 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return categoriaRepository.save(obj);
+	}
 }
