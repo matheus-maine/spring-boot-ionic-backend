87c98f3cb42e3c8db2c60962dab12056b03de7e2
Matheus Maine <matheusengcomp@hotmail.com>
2020-07-28 16:48:51 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/matheusmaine/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/matheusmaine/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +44,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> update (@PathVariable Integer id,@RequestBody Categoria obj) throws ObjectNotFoundException{
 		obj.setId(id);
 		obj = categoriaService.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete (@PathVariable Integer id) throws ObjectNotFoundException{
+		categoriaService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 
--- src/main/java/com/matheusmaine/cursomc/services/CategoriaService.java
+++ src/main/java/com/matheusmaine/cursomc/services/CategoriaService.java
@@ -3,20 +3,22 @@ package com.matheusmaine.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.matheusmaine.cursomc.domain.Categoria;
 import com.matheusmaine.cursomc.repositories.CategoriaRepository;
+import com.matheusmaine.cursomc.services.exceptions.DataIntegrityViolation;
 
 import javassist.tools.rmi.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository; // injeção de dependencia
-	
-	//usando Spring boot 2.xxx
+
+	// usando Spring boot 2.xxx
 	public Categoria find(Integer id) throws ObjectNotFoundException {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -27,9 +29,18 @@ public class CategoriaService {
 		obj.setId(null);
 		return categoriaRepository.save(obj);
 	}
-	
-	public Categoria update (Categoria obj) throws ObjectNotFoundException {
+
+	public Categoria update(Categoria obj) throws ObjectNotFoundException {
 		find(obj.getId());
 		return categoriaRepository.save(obj);
 	}
-} 
+
+	public void delete(Integer id) throws ObjectNotFoundException {
+		find(id);
+		try {
+			categoriaRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolation("Não é possivel excluir uma categoria que possui produtos!");
+		}
+	}
+}
