167cf0a3f73308372bb4575a98cbde427c192ea9
Matheus Maine Pereira <matheusengcomp@hotmail.com>
2020-09-11 13:18:21 -0300

    programando

--- src/main/java/com/matheusmaine/cursomc/resources/ClienteResource.java
+++ src/main/java/com/matheusmaine/cursomc/resources/ClienteResource.java
@@ -1,13 +1,24 @@
 package com.matheusmaine.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheusmaine.cursomc.domain.Cliente;
+import com.matheusmaine.cursomc.dto.ClienteDTO;
 import com.matheusmaine.cursomc.services.ClienteService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -17,12 +28,44 @@ import javassist.tools.rmi.ObjectNotFoundException;
 public class ClienteResource {
 
 	@Autowired
-	private ClienteService clienteService;
+	private ClienteService service;
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) throws ObjectNotFoundException {
-		Cliente obj = clienteService.find(id);
+		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update (@PathVariable Integer id, @Valid @RequestBody ClienteDTO objDto) throws ObjectNotFoundException{
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete (@PathVariable Integer id) throws ObjectNotFoundException{
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping
+	public ResponseEntity<List<ClienteDTO>> findAll() throws ObjectNotFoundException {
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome")String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC")String direction) throws ObjectNotFoundException {
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
--- src/main/java/com/matheusmaine/cursomc/services/ClienteService.java
+++ src/main/java/com/matheusmaine/cursomc/services/ClienteService.java
@@ -1,12 +1,20 @@
 package com.matheusmaine.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.matheusmaine.cursomc.domain.Cliente;
+import com.matheusmaine.cursomc.domain.Cliente;
+import com.matheusmaine.cursomc.dto.ClienteDTO;
 import com.matheusmaine.cursomc.repositories.ClienteRepository;
+import com.matheusmaine.cursomc.services.exceptions.DataIntegrityViolation;
 
 import javassist.tools.rmi.ObjectNotFoundException;
 
@@ -14,13 +22,40 @@ import javassist.tools.rmi.ObjectNotFoundException;
 public class ClienteService {
 	
 	@Autowired
-	private ClienteRepository clienteRepository; // injeção de dependencia
+	private ClienteRepository repo; // injeção de dependencia
 	
 	//usando Spring boot 2.xxx
 	public Cliente find(Integer id) throws ObjectNotFoundException {
-		Optional<Cliente> obj = clienteRepository.findById(id);
+		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente obj) throws ObjectNotFoundException {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void delete(Integer id) throws ObjectNotFoundException {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolation("Não é possivel excluir uma categoria que possui produtos!");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(),objDto.getNome(),objDto.getEmail(), null, null);
 	}
 
 }
